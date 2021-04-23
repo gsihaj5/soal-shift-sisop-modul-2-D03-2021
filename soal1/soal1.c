a7059e5f1416d0e42ee029d951f4a20132d7b641
widigda_cahya <62281277+widigdacahya@users.noreply.github.com>
2021-04-23 20:22:06 +0700

    update revisi nomer 1

--- soal1/soal1.c
+++ soal1/soal1.c
@@ -1,277 +1,178 @@
-// [belum berjalan di latar belakang]
-#include <stdlib.h>
 #include <sys/types.h>
-#include <unistd.h>
+#include <sys/stat.h>
 #include <stdio.h>
-#include <wait.h>
+#include <stdlib.h>
+#include <fcntl.h>
+#include <errno.h>
+#include <unistd.h>
+#include <syslog.h>
+#include <string.h>
+#include <sys/wait.h>
+#include <time.h>
 
-//[ untuk download foto ]
-void func_donwload_foto() {    
+/*
+//[step] 
+1. download zip file
+2. extract zip file
+3. rename foldernya
+4. zip folders to one
+5. delete folder
+*/
+
+// [proses execv]
+void execvProc(char commandPath[], char *argv[]) {
+    pid_t childId;
+    int status;
+
+    childId = fork();
+
+    if (childId < 0) {
+        while(childId < 0) {
+            childId = fork();
+        }
+            
+        exit(EXIT_FAILURE);
+    }
     
-    //download foto 
-    char *argv[]={"wget", "--no-check-certificate", "https://drive.google.com/uc?id=1FsrAzb9B5ixooGUs0dGiBr-rC7TS9wTD&export=download", "-O", "foto.zip",  NULL};
-    execv("/usr/bin/wget", argv);   
+    if (childId == 0) {
+        // child
+        
+        execv(commandPath, argv);
+        exit(0);
+
+    } else {
+        //parent
+        
+        while ((wait(&status)) > 0);
+    }
 }
 
-// [ untuk download musik ]
-void func_download_musik() {
-
-    //download musik
-    char *args[]={"wget", "--no-check-certificate", "https://drive.google.com/uc?id=1ZG8nRBRPquhYXq_sISdsVcXx5VdEgi-J&export=download", "-O", "musik.zip",  NULL};
-    execv("/usr/bin/wget", args);
-
+//[donwload]
+void downloadFileLink(char downloadLink[], char nameExt[]) {
+    char *argv[] = {"wget", "--no-check-certificate", downloadLink, "-O", nameExt, NULL};
+    execvProc("/usr/bin/wget", argv);
 }
 
-// [ untuk downlaod film ]
-void func_download_film() {
+//[extract]
+void extractZip(char filename[]) {
+    char *argv[] = {"unzip", filename, NULL};
+    execvProc("/usr/bin/unzip", argv);
+}
+
+//[rename]
+void renameFolder(char current[], char result[]) {
+    char *argv[] = {"mv", current, result, NULL};
+    execvProc("/bin/mv", argv);
+}
+
+//[zip to one]
+void zipToOne() {
+    char *argv[] = {"zip", "-r", "Lopyu_Stevany.zip", "Fylm", "Pyoto", "Myusik", NULL};
+    execvProc("/usr/bin/zip", argv);
+}
+
+//[delete folder]
+void deleteFolder(char folderName[]) {
+    char *argv[] = {"rm", "-r", folderName, NULL};
+    execvProc("/bin/rm", argv);
+}
+
+void processingFilm() {
+    downloadFileLink("https://drive.google.com/uc?id=1ktjGgDkL0nNpY-vT7rT7O6ZI47Ke9xcp&export=download", "film.zip");
+    extractZip("film.zip");
+    renameFolder("FILM", "Fylm");
+}  
+
+void processingMusik() {
+    downloadFileLink("https://drive.google.com/uc?id=1ZG8nRBRPquhYXq_sISdsVcXx5VdEgi-J&export=download", "musik.zip");   
+    extractZip("musik.zip");
+    renameFolder("MUSIK", "Myusik");
+}
+
+void processingFoto() {
+    downloadFileLink("https://drive.google.com/uc?id=1FsrAzb9B5ixooGUs0dGiBr-rC7TS9wTD&export=download", "foto.zip");
+    extractZip("foto.zip");
+    renameFolder("FOTO", "Pyoto");
+}
+
+void processTask(int i) {
     
-    //download film
-    char *argc[]={"wget", "--no-check-certificate", "https://drive.google.com/uc?id=1ktjGgDkL0nNpY-vT7rT7O6ZI47Ke9xcp&export=download", "-O", "film.zip",  NULL};
-    execv("/usr/bin/wget", argc);
-}
-
-void unzip_foto() {
-    //unzip foto 
-    char *argv[]={"unzip", "foto.zip", NULL};
-    execv("/usr/bin/unzip", argv);
-}
-
-void unzip_musik() {
-
-    //unzip musik 
-    char *argv[]={"unzip", "musik.zip", NULL};
-    execv("/usr/bin/unzip", argv);   
-
-}
-
-void unzip_film() {
-
-    //unzip film 
-    char *argv[]={"unzip", "film.zip", NULL};
-    execv("/usr/bin/unzip", argv);
-
-}
-
-void rename_to_pyoto(){
-
-    //rename FOTO -> Pyoto
-    char *argv[]={"mv", "FOTO", "Pyoto", NULL};
-    execv("/bin/mv", argv);
-}
-
-void rename_to_Musyik(){
-
-    //rename MUSIK -> Myusik
-    char *argv[]={"mv", "MUSIK", "Musyik", NULL};
-    execv("/bin/mv", argv); 
-}
-
-void rename_to_Fylm(){
-
-    //rename FILM -> Fylm
-    char *argv[]={"mv", "FILM", "Fylm", NULL};
-    execv("/bin/mv", argv);
+    if(i == 0) {
+        processingFoto();
+    } 
     
-}
-
-void zipto_one() {
-    //ZIP Pyoto,Fylm, Musyik ke satu zip
-    char *argv[]={"zip", "-r" ,"Lopyu_Stevany.zip" , "Pyoto", "Musyik", "Fylm", NULL};
-    execv("/usr/bin/zip", argv);
-}
-
-void delete_Musyik() {
-    char *argv[]={"rm", "-r", "Musyik", NULL};
-    execv("/bin/rm", argv);
-}
-
-void delete_Fylm() {
-    char *argv[]={"rm", "-r", "Fylm", NULL};
-    execv("/bin/rm", argv);
+    if(i == 1) {
+        processingMusik();
+    } 
     
+    if(i == 2) {
+        processingFilm();
+    }
+
 }
 
-void delete_Pyoto() {
-    char *argv[]={"rm", "-r", "Pyoto", NULL};
-    execv("/bin/rm", argv);
-}
 
 
-void func2() {
-  int i;
-  for (i = 51; i < 101; i++) {
-    printf("%d\n", i);
-  }
-}
 
 int main() {
-  pid_t child_id;
-  int status;
+    pid_t pid, sid;        // Variabel untuk menyimpan PID
 
-  child_id = fork();
-  if (child_id < 0) {
-    exit(EXIT_FAILURE);
-  }
+    pid = fork();     // Menyimpan PID dari Child Process
 
-  if (child_id == 0) {
-    //func_donwload_foto();
-    
-    //**dieksekusi jam 16:22 [belum mengetahui cara untuk melakukan hal tersebut]
-    pid_t child_download_1;
-    int stat_1;
-
-    child_download_1 = fork();
-    if(child_download_1<0) {
+    /* Keluar saat fork gagal
+    * (nilai variabel pid < 0) */
+    if (pid < 0) {
         exit(EXIT_FAILURE);
     }
 
-    if(child_download_1 == 0) {
-        //func_download_musik();
+    /* Keluar saat fork berhasil
+    * (nilai variabel pid adalah PID dari child process) */
+    if (pid > 0) {
+        exit(EXIT_SUCCESS);
+    }
 
-        pid_t child_download_2;
-        int stat_2;
+    umask(0);
 
-        child_download_2 = fork();
-        if (child_download_2==0) {
+    sid = setsid();
+    if (sid < 0) {
+        exit(EXIT_FAILURE);
+    }
 
-            pid_t child_download_3;
-            int stat_3;
+    ///         /home/catycahyul/Documents/seslab/seslab2/1_ini_revisi_fix/soal1_revisi.c
+    //          /home/catycahyul/Documents/seslab/seslab2/revisi/ver/try/soal1_revisi.c
+    if ((chdir("/home/catycahyul/Documents/seslab/seslab2/revisi/vers/try")) < 0) {
+        exit(EXIT_FAILURE);
+    }
 
-            child_download_3=fork();
-            if(child_download_3==0) {
+    close(STDIN_FILENO);
+    close(STDOUT_FILENO);
+    close(STDERR_FILENO);
 
-                pid_t child_4;
-                int stat_4;
-
-                child_4 = fork();
-                if(child_4==0) {
-                    
-                    pid_t child_5;
-                    int stat_5;
-
-                    child_5 = fork();
-                    if (child_5 == 0) {
-
-                        pid_t child_6;
-                        int stat_6;
-
-                        child_6 = fork();
-                        if(child_6==0) {
-
-                            pid_t child_7;
-                            int stat_7;
-
-                            child_7=fork();
-                            if (child_7==0) {
-
-
-                                pid_t child_8;
-                                int stat_8;
-
-                                child_8=fork();
-                                if (child_8==0) {
-                                    
-                                    pid_t child_9;
-                                    int stat_9;
-
-                                    child_9=fork();
-                                    if(child_9==0) {
-                                        func_donwload_foto();
-                                    } else {
-                                        while( wait(&stat_9)> 0 );
-                                        func_download_film();
-                                    }
-
-
-                                } else {
-                                    while( wait(&stat_8)> 0 );
-                                    func_download_musik();
-                                }
-                                
-
-                                
-                            } else {
-                                while( wait(&stat_7)> 0 );
-                                unzip_foto();
-                            }
-                            
-
-
-                        } else {
-                            while( wait(&stat_6)> 0 );
-                            unzip_film();
-                        }
-
-
-                    } else {
-                        while( wait(&stat_5)> 0 );    
-                        unzip_musik();
-                    }
-
-
-                }else {
-                    while( wait(&stat_4)> 0 );
-                    rename_to_pyoto();
-                }
- 
-
-            } else {
-                while( wait(&stat_3)> 0 );
-                rename_to_Musyik();
-            }
-
-
-            
-        } else {
-            while( wait(&stat_2)> 0 );
-            rename_to_Fylm();
-        }
+    while (1) {
+        time_t t = time(NULL);
+        struct tm timeInfo = *localtime(&t);
+        int i;
         
+        if(timeInfo.tm_mday == 9 && timeInfo.tm_mon + 1 == 4 && timeInfo.tm_hour == 16 && timeInfo.tm_min == 22 && timeInfo.tm_sec == 00) {
+            
+            for(i=0; i<3; i++) {
 
-
-    } else {
-        while( wait(&stat_1)> 0 );
-        //func_download_film();
-    }
-
-  } else {
-    while((wait(&status))> 0);
-    //func2();
-    //zipto_one();
-    // dikesekusi jam 22.22 [belum mengetahui cara untuk melakukan hal tersebut]
-    pid_t anak_1;
-    int statAnak_1;
-
-    anak_1=fork();
-    if(anak_1==0) {
-
-        pid_t anak_2;
-        int statAnak_2;
-
-        anak_2=fork();
-        if(anak_2==0) {
-
-            pid_t anak_3;
-            int statAnak_3;
-
-            anak_3 = fork();
-            if(anak_3==0) {
-                zipto_one();
-            } else {
-                while(wait(&statAnak_3)>0);
-                delete_Fylm();
+                if(fork() == 0) {
+                    processTask(i);
+                    exit(0);
+                }
             }
-
-        } else {
-            while(wait(&statAnak_2)>0);
-            delete_Musyik();
+                  
+            
+        } else if(timeInfo.tm_mday == 9 && timeInfo.tm_mon + 1 == 4 && timeInfo.tm_hour == 22 && timeInfo.tm_min == 22 && timeInfo.tm_sec == 00) {
+            zipToOne();
+            deleteFolder("Fylm");
+            deleteFolder("Myusik");
+            deleteFolder("Pyoto");
         }
 
-    } else {
-        while(wait(&statAnak_1)>0);
-        delete_Pyoto();
+        sleep(1);
     }
-
     
-  }
+    exit(0);
 }
